0c243d9495098b57e61512e1469e511ae70af01a
Michael Harley <michael@mharley.dev>
2021-01-07 19:42:20 -0600

    Add backblaze

--- config.sh
+++ config.sh
@@ -218,7 +218,8 @@ printf "%s\n  blockblock\n%s" $yellow $end
 brew cask install blockblock
 printf "%s\n  joplin\n%s" $yellow $end
 brew cask install joplin
-
+printf "%s\n  backblaze\n%s" $yellow $end
+brew cask install backblaze
 
 # Remove outdated versions from the cellar.
 printf "%s\n  brew cleanup\n%s" $yellow $end
